a596444825209b6d01d0491f6d65b3a4b4042eb7
daniel-burlacu <daniel.burlacu1983@yahoo.se>
2024-12-26 00:40:50 +0100

    Adding logic for the ACTIONS_CORS_HEADERS

--- src/app/api/actions/route.ts
+++ src/app/api/actions/route.ts
@@ -172,16 +172,18 @@ export async function POST(request: Request) {
 }
 
 export async function OPTIONS(request: Request) {
-  // Respond to the preflight request with CORS headers
   return new Response(null, {
       headers: {
-          "Access-Control-Allow-Origin": "*", // Replace "*" with the specific origin if needed
+          "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
           "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Access-Control-Max-Age": "86400", // Cache the preflight response for 24 hours
+          "Access-Control-Max-Age": "86400",
+          "X-Action-Version": "1",
+          "X-Blockchain-Ids": "solana:devnet",
       },
   });
 }
+
 
 // function validatedQueryParams(requestUrl: URL) {
 //   let toPubkey: PublicKey = DEFAULT_SOL_ADDRESS;
