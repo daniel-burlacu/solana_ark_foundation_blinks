f91be0b443431995dfe8fd3a0853a23ce1a01a7a
daniel-burlacu <daniel.burlacu1983@yahoo.se>
2024-12-26 00:23:07 +0100

    Adding logic for the ACTIONS_CORS_HEADERS

--- src/app/api/actions/const.ts
+++ src/app/api/actions/const.ts
@@ -10,13 +10,15 @@ export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
 //   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 // };
 
+const CLUSTER = process.env.NEXT_PUBLIC_CLUSTER || "devnet"; // Use "mainnet" for production
 export const ACTIONS_CORS_HEADERS = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  "X-Action-Version": "1", // Set the action version to "1" as required by Dialect
-  "X-Blockchain-Ids": "solana:devnet", // Use "solana:mainnet" for production
+  "X-Action-Version": "1",
+  "X-Blockchain-Ids": `solana:${CLUSTER}`, // Dynamically set based on the cluster
 };
+
 
 
 export const DEFAULT_SOL_AMOUNT: number = 1.0;
