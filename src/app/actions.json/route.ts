254575a7efcfbf600ee055e9c6bba966741891b8
daniel-burlacu <daniel.burlacu1983@yahoo.se>
2025-01-13 18:18:40 +0100

    adding change to routes.ts in actions.json

--- src/app/actions.json/route.ts
+++ src/app/actions.json/route.ts
@@ -1,15 +1,4 @@
-import {  ActionsJson } from "@solana/actions";
-
-const ACTIONS_CORS_HEADERS = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    "Access-Control-Max-Age": "86400",
-    "X-Action-Version": "1",
-    "X-Blockchain-Ids":"solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1", //mainnet - solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp 
-    "chainId":"devnet"
-   // "X-Blockchain-Ids": `solana:${CLUSTER}`, // Dynamically set based on the cluster
-  };
+import {  ACTIONS_CORS_HEADERS, ActionsJson } from "@solana/actions";
 
 export const GET = async () => {
   const payload: ActionsJson = {
