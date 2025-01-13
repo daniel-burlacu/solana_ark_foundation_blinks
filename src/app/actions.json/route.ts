cd76125dc3bfd54c84feba1203b1f355eda275c2
daniel-burlacu <daniel.burlacu1983@yahoo.se>
2025-01-13 18:36:02 +0100

    adding cors changes

--- src/app/actions.json/route.ts
+++ src/app/actions.json/route.ts
@@ -1,4 +1,15 @@
-import {  ACTIONS_CORS_HEADERS, ActionsJson } from "@solana/actions";
+import {  ActionsJson } from "@solana/actions";
+
+const ACTIONS_CORS_HEADERS = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Max-Age": "86400",
+    "X-Action-Version": "1",
+    "X-Blockchain-Ids":"solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1", //mainnet - solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp 
+    "chainId":"devnet"
+   // "X-Blockchain-Ids": `solana:${CLUSTER}`, // Dynamically set based on the cluster
+  };
 
 export const GET = async () => {
   const payload: ActionsJson = {
@@ -23,7 +34,6 @@ export const GET = async () => {
 
 // DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
 // THIS WILL ENSURE CORS WORKS FOR BLINKS
-// Handle OPTIONS requests for preflight
 export const OPTIONS = () => {
   return Response.json(null, {
       status: 204,
