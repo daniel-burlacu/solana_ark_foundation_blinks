7190271d0228ac0b881fb8a293e5902734599874
daniel-burlacu <daniel.burlacu1983@yahoo.se>
2025-01-14 08:55:17 +0100

    actions.json routes changed

--- src/app/actions.json/route.ts
+++ src/app/actions.json/route.ts
@@ -1,16 +1,6 @@
 import {  ActionsJson } from "@solana/actions";
 import { NextRequest, NextResponse } from "next/server";
-
-const ACTIONS_CORS_HEADERS = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    "Access-Control-Max-Age": "86400",
-    "X-Action-Version": "1",
-    "X-Blockchain-Ids":"solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1", //mainnet - solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp 
-    "chainId":"devnet"
-   // "X-Blockchain-Ids": `solana:${CLUSTER}`, // Dynamically set based on the cluster
-  };
+import { ACTIONS_CORS_HEADERS } from "../api/actions/const";
 
 export const GET = async () => {
   const payload: ActionsJson = {
