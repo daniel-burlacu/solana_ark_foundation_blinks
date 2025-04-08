c04fc79e1e3b07cb9d211f154bf39a630fdd2779
daniel-burlacu <daniel.burlacu1983@yahoo.se>
2025-04-08 09:12:50 +0200

    adding new links for the images in the app

--- src/app/api/actions/route.ts
+++ src/app/api/actions/route.ts
@@ -55,7 +55,7 @@ export async function GET(request: Request) {
 
   const responseBody: ActionGetResponse = transactionCompleted
     ? {
-        icon: "https://bafkreibllcqfjk5ch26tdq7sqotkq3xxlymivip6ta7rdjhaf2qccnzc7u.ipfs.flk-ipfs.xyz",
+        icon: "https://devnet.irys.xyz/FRK2U41TQSv2tnKzBtuY3LkhqSM6q8GrvbQQJyvVSrut",
         description: "Thank you for your donation! You can now mint your NFT.",
         title: "Solana Ark Foundation Supporter",
         label: "Mint an NFT",
@@ -71,7 +71,7 @@ export async function GET(request: Request) {
       }
     : {
         //icon: "https://bafybeibqfafl757oc2ts3dnyxpapq7fthx2og2kod4cd3yeysm7q6hxaxq.ipfs.flk-ipfs.xyz",
-        icon: "https://bafkreibllcqfjk5ch26tdq7sqotkq3xxlymivip6ta7rdjhaf2qccnzc7u.ipfs.flk-ipfs.xyz",
+        icon: "https://devnet.irys.xyz/FRK2U41TQSv2tnKzBtuY3LkhqSM6q8GrvbQQJyvVSrut",
         description:
           "The time to act is now! ... before their silence becomes our legacy.",
         title: "Solana Ark Foundation Supporter",
@@ -205,7 +205,7 @@ export async function POST(request: Request) {
             type: "inline",
             action: {
               type: "action",
-              icon: "https://bafybeibqfafl757oc2ts3dnyxpapq7fthx2og2kod4cd3yeysm7q6hxaxq.ipfs.flk-ipfs.xyz",
+              icon: "https://devnet.irys.xyz/ERXUytdJNnNGXHkTFbKBMaHe6dQbTE36cuXtgCxw2fgy",
               label: "Mint NFT",
               title: "Mint SAF Supporter Badge NFT",
               disabled: false,
@@ -297,7 +297,7 @@ return Response.json(responseBody, { headers: ACTIONS_CORS_HEADERS });
               type: "inline",
               action: {
                 type: "action",
-                icon: "https://bafybeibqfafl757oc2ts3dnyxpapq7fthx2og2kod4cd3yeysm7q6hxaxq.ipfs.flk-ipfs.xyz",
+                icon: "https://devnet.irys.xyz/ERXUytdJNnNGXHkTFbKBMaHe6dQbTE36cuXtgCxw2fgy",
                 label: "NFT Minted completed !",
                 title: "NFT Minted Successfully !",
                 disabled: false,
--- src/app/api/nft/nft_image.ts
+++ src/app/api/nft/nft_image.ts
@@ -1,17 +1,17 @@
-// import wallet from "/home/daniel/.solana/.config/keypari.json";
+import wallet from "/home/daniel/.solana/.config/keypari.json";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { createGenericFile} from "@metaplex-foundation/umi";
+import { createGenericFile, createSignerFromKeypair, signerIdentity} from "@metaplex-foundation/umi";
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
 import { readFile } from "fs/promises";
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
 
-// let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
-// const signer = createSignerFromKeypair(umi, keypair);
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
 
 umi.use(irysUploader());
-// umi.use(signerIdentity(signer));
+umi.use(signerIdentity(signer));
 
 (async () => {
     try {
@@ -19,7 +19,8 @@ umi.use(irysUploader());
         //2. Convert image to generic file.
         //3. Upload image
 
-        const image = await readFile("/home/daniel/turbin3/capstone/Q4_SOL_DanielB/ts/cluster1/image/SolanaSupporter.png");
+//        const image = await readFile("/home/daniel/turbin3/capstone/Q4_SOL_DanielB/ts/cluster1/image/SolanaSupporter.png");
+        const image = await readFile("/home/daniel/turbin3/capstone/Q4_SOL_DanielB/ts/cluster1/image/SolanaArkFoundationLogo.png");
         const genericFile = createGenericFile(image, 'NFT-Dogpound-Charity',{contentType: 'image/png'});
         const [myUri] = await umi.uploader.upload([genericFile]); 
         
